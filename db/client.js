d5278573da431faffbf7cb245f58e877780fef0e
raymond triana <raymondtriana@gmail.com>
2023-12-08 15:04:39 -0500

    established connection with the db

--- db/client.js
+++ db/client.js
@@ -1,10 +1,13 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/gamestore';
+const connectionString = process.env.DATABASE_URL || "https://postgres:password@localhost:5432/gamestore";
 
 const client = new Client({
   connectionString,
+  user:"postgres",
+  password:'',
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
+
 
 module.exports = client;
